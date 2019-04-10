8600c3a08fb049a3e2f7dde69b26be9ff8da8a14
Tumisho Rachuene <1314101@students.wits.ac.za>
2019-04-10 12:02:14 +0200

    The routing port added

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ let app = express();
 let mainRouter = require("./mainRoutes");
 
 app.use(mainRouter);
-app.listen(3000);
-console.log("Express server running on port 3000");
+let port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Express server running on port", port);
